381f52a7f3ee3955aad479cf9ac49ccb16899d98
NataRey <lenarero@gmail.com>
2025-08-16 10:55:23 -0500

    taller de conocimientos

---
+++ angie.js
@@ -0,0 +1,72 @@
+// --- Taller de Operadores ---
+
+// 1. Operadores de Asignación
+// Declara una variable llamada 'nombre' y asígnale tu nombre.
+
+
+// 2. Operadores Aritméticos
+// a) Crea una variable 'suma' y guarda la suma de 5 y 10.
+
+
+// b) Crea una variable 'resta' y guarda la resta de 25 y 7.
+
+
+// c) Crea una variable 'multiplicacion' y guarda el resultado de 4 por 6.
+
+
+// d) Crea una variable 'division' y guarda el resultado de 20 entre 4.
+
+
+// e) Crea una variable 'residuo' y guarda el residuo de 17 dividido entre 5.
+
+
+// 3. Operadores de Comparación
+// Completa para que el resultado en la consola sea true. quita el comentario de la consola como en el primer ejemplo
+console.log(10 < 5);      // Mayor que
+// console.log(8 ... 8);       // Igual estricto
+// console.log(9 ... 10);      // Menor que
+// console.log(5 ... "5");     // Igual (no estricto)
+
+// 4. Operadores Lógicos
+// Completa para que el resultado en la consola sea 'true' en el primer caso y 'false' en el segundo.
+// console.log((5 > 3)  (10 < 20)); // AND(Y)
+//console.log((10 > 20) ... (5 == 5)); // OR(O)
+
+// 5. Operador de Concatenación
+// Crea una variable 'fraseCompleta' que una las siguientes dos palabras:
+let palabra1 = "Mi nombre es ";
+let nombre = "Angie"
+let palabra2 = " y seré una desarrolladora exitosa";
+
+
+// 6. Práctica con Operaciones Específicas
+// El objetivo es que las operaciones resulten en los valores indicados en los comentarios.
+
+// a) Operación de Suma
+// Crea una variable llamada 'resultadoSuma' y guarda el resultado de una suma
+// que dé como resultado 15.
+// Usa console.log() para verificar el resultado.
+
+
+// b) Operación de Resta
+// Crea una variable llamada 'resultadoResta' y guarda el resultado de una resta
+// que dé como resultado 5.
+// Usa console.log() para verificar el resultado.
+
+
+// c) Operación de Multiplicación
+// Crea una variable llamada 'resultadoMultiplicacion' y guarda el resultado de una multiplicación
+// que dé como resultado 30.
+// Usa console.log() para verificar el resultado.
+
+
+// d) Operación de División
+// Crea una variable llamada 'resultadoDivision' y guarda el resultado de una división
+// que dé como resultado 4.
+// Usa console.log() para verificar el resultado.
+
+
+// e) Operación de Módulo
+// Crea una variable llamada 'resultadoModulo' y guarda el residuo de una división
+// que dé como resultado 1.
+// Usa console.log() para verificar el resultado.
